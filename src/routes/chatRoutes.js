1974d8432fa2fc778f73cc58f5c067d615f85267
DR <rudranarayanmohapatra87878@gmail.com>
2026-05-02 13:08:38 +0530

    Chat deleted.

--- src/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMessages, sendMessage } = require('../controllers/chatController');
-
-// No middleware (protect) for simple project demo
-router.post('/messages', getMessages);
-router.post('/send', sendMessage);
-
-module.exports = router;
